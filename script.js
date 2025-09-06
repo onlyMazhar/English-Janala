8f39e68259fb0afecb7e4289e4067eb0e498e4a8
Mazhar <mi.sabbir420@gmail.com>
2025-09-06 17:05:59 +0600

    level words adding

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
 const loadLeasons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((response) => response.json()) //
+    .then((json) => displayLessons(json.data)); //
+};
 
-    fetch("https://openapi.programming-hero.com/api/levels/all")
-        .then((response) => response.json())
-        .then((json) => displayLessons(json.data))
+const loadWordLevel = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then(res=> res.json())
+    .then(json => displayWordLevel(json.data))
+};
+
+
+const displayWordLevel = (words) => {
+    console.log(words)
+    const loadWordLevelContainer = document.getElementById('loadWordLevelContainer');
+    loadWordLevelContainer.innerHTML = '';
+    
+    words.forEach(word => {
+        const wordDiv = document.createElement('div');
+        wordDiv.innerHTML = `
+        <p >${word.word} = ${word.meaning}</p>
+        `
+        loadWordLevelContainer.appendChild(wordDiv)
+    });
+
+
+
 }
 
 const displayLessons = (leasons) => {
-    // console.log(leasons)
+  // console.log(leasons)
 
-    // 1 get the perent container and empyt container with ''
-    const container = document.getElementById('level-container');
-    container.innerHTML = '';
+  // 1 get the perent container and empyt container with ''
+  const container = document.getElementById("level-container");
+  container.innerHTML = "";
 
-    // 2 get every element
-    for (const leason of leasons) {
-        console.log(leason)
+  // 2 get every element
+  for (const leason of leasons) {
+    // console.log(leason);
 
-        // 3 create element
-        const btnDiv = document.createElement('div');
-        btnDiv.innerHTML = `
-                    <button class="btn btn btn-outline btn-primary ">
+    // 3 create element
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+                    <button onClick ="loadWordLevel(${leason.level_no})" class="btn btn btn-outline btn-primary ">
                     <i class="fa-solid fa-book-open"></i> Leason - ${leason.level_no}
                     </button>
-        `
+        `;
 
-        // 4 append chield in perent container
-        container.appendChild(btnDiv);
-    }
+    // 4 append chield in perent container
+    container.appendChild(btnDiv);
+  }
+};
 
-
-}
-loadLeasons()
+loadLeasons();
